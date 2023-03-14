7a15d59dc332c1f91f2e741bc92228faa9ecc359
adammaher1 <B00148489@mytudublin.ie>
2023-03-14 19:54:26 +0000

    Updated database connected

---
+++ html/signup.php
@@ -0,0 +1,45 @@
+
+<link rel="stylesheet" type="text/css" href="../css/signup.css">
+<title>Sign in</title>
+</head>
+
+
+<body>
+<div class="container">
+    <form action="" method="post" name="Login_Form" class="form-signup">
+        <h2 class="form-signup-heading">Please sign up</h2>
+
+        <label for="inputUFirstname" >First Name</label>
+        <input name="Firstname" type="firstname" id="inputUFirstname" class="form-control" placeholder="Firstname" required autofocus>
+
+        <label for="inputSurname" >Surname</label>
+        <input name="Surname" type="surname" id="inputSurname" class="form-control" placeholder="Surname" required autofocus>
+
+
+        <label for="inputAddress" >Address</label>
+        <input name="Address" type="address" id="inputAddress" class="form-control" placeholder="Address" required autofocus>
+
+
+        <label for="inputPassword">Password</label>
+        <input name="Password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+        <label for="inputEmail" >Email</label>
+        <input name="Email" type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+
+
+
+
+
+
+
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button name="Submit" value="Signup" class="button" type="submit">Sign up</button>
+
+    </form>
+</div>
+</body>
+</html>
