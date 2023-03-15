5d015ba1d5dc097f2a03133a93c18b433f8f493f
peterk377 <peterk377@gmail.com>
2023-03-15 20:16:45 +0000

    added more show searches

---
+++ connectionmysqli/showSoccer.php
@@ -0,0 +1,24 @@
+<?php
+include "connectionsqli.php";
+
+
+$sql = "select * from item where Sport = 'Soccer'";  // $sql var to do select query
+$qryResult = mysqli_query($conn, $sql);  // $qry the result with the connection
+
+
+// If the results were found or not 0
+if (mysqli_num_rows($qryResult) > 0)
+{
+    // Fetch and echo the sql query results
+    while ($row = mysqli_fetch_assoc($qryResult)) {
+
+        echo "Item id = " . $row["itemID"] . " Price = " . $row["price"] . " image = " . $row["image"] . " Stock = " . $row["stock"] . " Sport = " . $row["Sport"] . "<br><br>";
+    }
+}
+else
+{
+    echo '0 results found';  // Print 0 found
+}
+
+
+mysqli_close($conn);   // Close the sql connection when done
