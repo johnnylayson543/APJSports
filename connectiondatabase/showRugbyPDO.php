47fde0137cb2bb30251eb0284c7b04d2c0a3078c
peterk377 <peterk377@gmail.com>
2023-03-26 12:55:36 +0100

    added rugby and swimming pdo

---
+++ connectiondatabase/showRugbyPDO.php
@@ -0,0 +1,17 @@
+<?php
+include "connection.php";
+
+
+
+$sql = "SELECT * FROM item WHERE Sport = 'Rugby'";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
+if ($stmt->rowCount() > 0) {
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<img src='../images/rugby/" . $row["image"] . "' width='250' height='250'>" .
+            "Item id = " . $row["itemID"] . " Price = " . $row["price"] . " Stock = " . $row["stock"] .
+            " Sport = " . $row["Sport"] . "<br><br>";
+    }
+
+}
