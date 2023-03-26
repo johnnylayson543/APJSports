b3540ae2f35537b36051bc800fd247e8be81406c
peterk377 <peterk377@gmail.com>
2023-03-26 12:23:48 +0100

    show basketball pdo working

--- connectiondatabase/connection.php
+++ connectiondatabase/connection.php
@@ -2,14 +2,14 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "password";
 
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=apjdatabase", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
--- connectiondatabase/showBasketballPDO.php
+++ connectiondatabase/showBasketballPDO.php
@@ -1,40 +1,17 @@
 <?php
-//include "connection.php";
-
-$servername = "localhost";
-$username = "root";
-$password = "password";
+include "connection.php";
 
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=apjdatabase", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
 
-    $sql = "SELECT * FROM item WHERE Sport = 'Basketball'";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
+$sql = "SELECT * FROM item WHERE Sport = 'Basketball'";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
 
-    echo 'this is pdo';
-
-    if ($stmt->rowCount() > 0) {
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "<img src='../images/basketball/" . $row["image"] . "' width='250' height='250'>" .
-                "Item id = " . $row["itemID"] . " Price = " . $row["price"] . " Stock = " . $row["stock"] .
-                " Sport = " . $row["Sport"] . "<br><br>";
-        }
-
+if ($stmt->rowCount() > 0) {
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<img src='../images/basketball/" . $row["image"] . "' width='250' height='250'>" .
+            "Item id = " . $row["itemID"] . " Price = " . $row["price"] . " Stock = " . $row["stock"] .
+            " Sport = " . $row["Sport"] . "<br><br>";
     }
 
-    else {
-        echo '0 results found';
-    }
-
-    $pdo = null;
-
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
 }
-
-
