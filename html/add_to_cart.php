713102826ca1c75a020d6e6c02b8a55dad72f66a
adammaher1 <B00148489@mytudublin.ie>
2023-04-15 17:03:36 +0100

    Header item fixed

--- html/add_to_cart.php
+++ html/add_to_cart.php
@@ -12,8 +12,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["addToCart"])) {
     $_SESSION ["cart"][$sport] = $sport;
     $_SESSION ["cart"][$price] = $price;
 
+    // Get the URL of the previous page
+    $previousPage = $_SERVER['HTTP_REFERER'];
 
-    header("Location: cart.php");
+    // Redirect back to the previous page
+    header("Location: $previousPage");
+    
     exit;
 }
 ?>
