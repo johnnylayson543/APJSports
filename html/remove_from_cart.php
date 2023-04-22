6464875dd8aea04d56f5d8286fcfb22a0f1662c8
peterk377 <peterk377@gmail.com>
2023-04-22 13:24:07 +0100

    removing items from cart by 1

--- html/remove_from_cart.php
+++ html/remove_from_cart.php
@@ -1,12 +1,19 @@
 <?php
+
+require "../classes/Item.php";
+
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["removeFromCart"])) {
 
     $itemId = $_POST["itemId"];
 
+    $item = new Item($itemId,0,"",0,"");
+
     if (isset($_SESSION["cart"][$itemId])) {
-        unset($_SESSION["cart"][$itemId]); // Remove item from cart session
+        $_SESSION["cart"][$itemId]["quantity"]--;
+        //unset($_SESSION["cart"][$itemId]);// Remove item from cart session
+        $item->__incStock($itemId,1);
     }
 
     header("Location: cart.php");
